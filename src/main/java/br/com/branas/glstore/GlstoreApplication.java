fbee3af3394aa5f27b468bb940de22a4b655afc7
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-04-15 17:19:23 -0300

    commit inicial

---
+++ src/main/java/br/com/branas/glstore/GlstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.branas.glstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/branas/glstore/GlstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.branas.glstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
