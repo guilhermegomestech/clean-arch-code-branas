56ede885cf99c35d15a7f4ed7a64e004cc646cb4
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-06-28 09:01:23 -0300

    refactor(card3/parte2) : refactor class test
    fixes #card3 - parte 2 - Ao fazer um pedido, o mesmo item não pode ser informado mais de uma vez

--- src/test/java/br/com/branas/glstore/services/OrderServiceTests.java
+++ src/test/java/br/com/branas/glstore/services/OrderServiceTests.java
@@ -1,6 +1,5 @@
 package br.com.branas.glstore.order;
 
-import br.com.branas.glstore.domain.entities.Order;
 import br.com.branas.glstore.domain.services.OrderService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,10 +15,12 @@ public class OrderServiceTests {
 
     @InjectMocks
     private OrderService orderService;
+
     @Test
     public void testCpfInvalid() {
         assertFalse(orderService.validateCpf("000.531.410-50"));
     }
+
     @Test
     public void testCpfValid() {
         assertTrue(orderService.validateCpf("407.302.170-27"));
