bc13fb4f7ac92a81f8dc4dc520fccf85372a0b9d
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-07-23 13:45:57 -0300

    feat(hateaos) : implementation hateoas in boundary OrderController
    fixes #Course clean code and clean architecture

--- src/main/java/br/com/branas/glstore/application/OrderController.java
+++ src/main/java/br/com/branas/glstore/application/OrderController.java
@@ -1,17 +1,25 @@
-package br.com.branas.glstore.application.api;
+package br.com.branas.glstore.application;
 
 import br.com.branas.glstore.domain.entities.Order;
 import br.com.branas.glstore.domain.services.OrderService;
 import br.com.branas.glstore.infrastructure.exceptions.OrderException;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 //Boundary
 @RestController
-@RequestMapping("api/v1/pedido")
+@RequestMapping("api/v1/order")
 public class OrderController {
+
+    private static final String REL_FOR_GET_ALL_ORDER = "Custumer order list";
+    private static final String MSG_NOT_FOUND_ORDER_SERIAL_NUMBER = "Order whit number informated not found";
 
     private final OrderService orderService;
 
@@ -20,23 +28,51 @@ public class OrderController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Order>> getAllPedidos(){
+    public ResponseEntity<List<Order>> getAllOrder(){
         List<Order> listaOrders = this.orderService.getAllPedidos();
-
         if(listaOrders.isEmpty()){
             return ResponseEntity.noContent().build();
         }
-
+        addHateoasInOrderList(listaOrders);
         return ResponseEntity.ok(this.orderService.getAllPedidos());
     }
 
+    private void addHateoasInOrderList(List<Order> listaOrders){
+        Link linkUri = null;
+        for (Order orderHateoas:listaOrders){
+            addLinkFindOrderSerialNumber(orderHateoas, linkUri);
+            addLinkGetAllOrder(orderHateoas, linkUri);
+        }
+    }
+    private void addLinkFindOrderSerialNumber(Order orderHateoas, Link linkUri){
+        linkUri = linkTo(methodOn(OrderController.class).findOrderBySerialNumber(orderHateoas.getSerialNumberOrder())).withSelfRel();
+        orderHateoas.add(linkUri);
+    }
+
+    private void addLinkGetAllOrder(Order orderHateoas, Link linkUri){
+        linkUri = linkTo(methodOn(OrderController.class).getAllOrder()).withRel(REL_FOR_GET_ALL_ORDER);
+        orderHateoas.add(linkUri);
+    }
+
     @PostMapping()
-    public ResponseEntity<?> criarPedido(@RequestBody Order order){
+    public ResponseEntity<?> createOrder(@RequestBody Order order){
         try{
             return ResponseEntity.ok(this.orderService.salvarPedido(order));
         } catch (OrderException oe){
             return ResponseEntity.unprocessableEntity().body(oe.getMessage());
         }
+    }
+
+    @GetMapping("/{serialNumber}")
+    public ResponseEntity<?> findOrderBySerialNumber(@PathVariable Long serialNumber){
+        Order order = orderService.getOrderBySerialNumberOrder(serialNumber);
+        if(order == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(MSG_NOT_FOUND_ORDER_SERIAL_NUMBER);
+        }
+
+        linkTo(OrderController.class).slash(order.getIdOrder()).withSelfRel();
+        order.add(linkTo(OrderController.class).slash(order).withSelfRel());
+        return ResponseEntity.ok(order);
     }
 }
 
